146b329bb9d25f833e89157b489589e3a6ed1acd
adiecr <adieassal@gmail.com>
2022-09-21 11:00:23 +0300

    added changes to graphPage

--- yscore/src/pages/GraphPage.jsx
+++ yscore/src/pages/GraphPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {VStack, Flex, Heading} from "@chakra-ui/layout"
 
 function GraphPage() {
   return (
-    <div >
-
-    </div>
+    <VStack >
+        <Flex>
+            <Heading>
+            Your <b>Score History</b>
+            </Heading>
+        </Flex>
+        <Flex>
+            View and monitor your credit history 
+        </Flex>
+    </VStack>
   )
 }
 
