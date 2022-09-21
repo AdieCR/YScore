70990282cf8b9b3bc4e9bb21cb021fd22cf21b91
RD <randahn03@gmail.com>
2022-09-22 00:08:20 +0300

    login/logout fully functional (FE + BE)

--- yscore/src/components/Navbar.jsx
+++ yscore/src/components/Navbar.jsx
@@ -1,27 +1,47 @@
-import {Button, Flex, useDisclosure } from "@chakra-ui/react";
+import { Button, Flex, useDisclosure } from "@chakra-ui/react";
 import React, { useState } from "react";
 import SignIn from "./SignIn";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import Menu from "./Menu";
 import { useUserContext } from "../contexts/userContext";
+import { logoutAxios } from "../lib/apiFuncrtionality";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const [showLoginModal, setShowLoginModal] = useState(false);
-  const {isUserLogged} = useUserContext();
+  const { setUserDetails, isUserLogged, setIsUserLogged } = useUserContext();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  
+  const navigate = useNavigate();
+
+
+  const handleLogout = async(e) => {
+    e.preventDefault();
+    const isLoggedOut = await logoutAxios();
+    console.log("isLoggedOut", isLoggedOut);
+    if (isLoggedOut) {
+      setIsUserLogged(false);
+      setShowLoginModal(false);
+      setUserDetails({});
+      navigate("/");
+    }
+  }
 
   return (
     <Flex p="1rem" justifyContent="space-between">
       <HamburgerIcon fontSize="2rem" onClick={onOpen} />
-      <Menu onClose={onClose} isOpen={isOpen}/>
+      <Menu onClose={onClose} isOpen={isOpen} />
 
-      {isUserLogged ? <Button>Log out</Button> :<><Button onClick={() => setShowLoginModal(true)}>Sign In/Up</Button>
+      {isUserLogged ? (
+        <Button onClick={(e) => handleLogout(e)}>Log out</Button>
+      ) : (
+        <>
+          <Button onClick={() => setShowLoginModal(true)}>Sign In/Up</Button>
+        </>
+      )}
       <SignIn
         showLoginModal={showLoginModal}
         setShowLoginModal={setShowLoginModal}
-      /></>}
-      
+      />
     </Flex>
   );
 }
