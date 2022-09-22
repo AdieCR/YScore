65414c010e119724d4010a24d8e3a13e620b191f
adiecr <adieassal@gmail.com>
2022-09-22 09:42:32 +0300

    fixed homepage

--- yscore/src/pages/Home.jsx
+++ yscore/src/pages/Home.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import { Box, VStack, Image } from "@chakra-ui/react";
+import { Box, VStack, Image, Heading, Flex } from "@chakra-ui/react";
 import Logo from "../images/Logo.svg";
 import styles from "../css/Home.module.css";
 
 function HomePage() {
   return (
     <>
-      <VStack className="div-container">
-        <Box bgColor={"#e0dedea1"}>
-          <Box className={styles["top-div"]}>
+      <VStack className={styles["div-container"]}>
+        <div className={styles["logo-container"]}>
+            <Image className={styles["logo"]} src={Logo} alt="logo" />
+        </div>
+            <Flex>
             <Box className={styles["top-div-content"]}>
-              <Box>
-                <Image className={styles["logo"]} src={Logo} alt="logo" />
+                <Heading>Get Your Credit Score</Heading>
+              <Box className={styles["text-div"]}>
+              Your credit score can have a big impact on your financial future. Sign up for YScore to get your credit score for free! Join us and get the tools you need to help understand, manage, and master your credit - in under 3 minutes. Checking your credit score with YScore won't hurt your score.
               </Box>
-                <Box className={styles["text-div"]} m={'auto'}>
-                Your credit score can have a big impact on your financial future. Sign up for YScore to get your credit score for free! Join us and get the tools you need to help understand, manage, and master your credit - in under 3 minutes. Checking your credit score with YScore won't hurt your score.
-              </Box>
+             
             </Box>
-          </Box>
-        </Box>
-        <Box className={styles["bottom-div"]}></Box>
+            <Image className={styles["home-pic"]} src="https://images.ctfassets.net/h0969ul5arsq/6o2njcGEnFXfgDSeqeoroA/7f27d1ae4190683352caa819c9c13bc2/Important_to_Establish_Credit_While_Young_-_Large_730x300.png" alt="logo" />
+            </Flex> 
+        
       </VStack>
     </>
   );
