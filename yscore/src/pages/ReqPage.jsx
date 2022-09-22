845b52d9acde32f7c0f20b8c96fc5a69a385e8fa
Myribi <105745901+Myribi@users.noreply.github.com>
2022-09-22 14:53:53 +0300

    end

--- yscore/src/pages/ReqPage.jsx
+++ yscore/src/pages/ReqPage.jsx
@@ -1,31 +1,44 @@
-import React from 'react'
-import {VStack, Flex, Heading} from "@chakra-ui/layout"
+import React from "react";
+import { VStack, Flex, Heading, Box } from "@chakra-ui/layout";
 
-import './GraphPage.css'
-import {Button} from '@chakra-ui/react'
+import "./GraphPage.css";
+import { Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 function GraphPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-    <VStack >
-        <Flex>
-            <Heading>
-            Recommendations for your credit Score
-            </Heading>
+    <VStack>
+      <Flex>
+        <Heading>Recommendations for your credit Score</Heading>
+      </Flex>
+
+      <Box className="alignGraph">What affects your credit score the most?</Box>
+
+      
+      <Flex direction="row" width="50%">
+        <Flex direction="column"  width="100%" mt="5rem">
+          <Box ml="auto" p="1rem" border="1px solid black" width="60%" bgColor="green" color="white">Income</Box>
+          <Box p="1rem" border="1px solid black" bgColor="green" color="white">Total account balance</Box>
         </Flex>
-        
-        <div className='alignGraph'>
-        What affects your credit score the most?
-        </div>
-        <div>
-            
-        </div>
-        <Button bgColor='#3DE6B9' size='md' className='buttonColor' onClick={() => navigate(-1)}>
-   Return
-  </Button>
+        <Flex direction="column"  width="100%" mt="8.6rem" mb="3rem">
+          <Box p="1rem" border="1px solid black" bgColor="red" color="white">Missing payements</Box>
+          <Box p="1rem"border="1px solid black" bgColor="red" color="white" width="60%">Application type</Box>
+        </Flex>
+      </Flex>
+
+      <Box boxShadow="base" p="6" rounded="md" bg="white" mb="2rem"> If your annual income is in range $20 000 - 40 000 and you apply for loan as a couple, your score will be 740.</Box>
+
+      <Button
+        bgColor="#3DE6B9"
+        size="md"
+        className="buttonColor"
+        onClick={() => navigate(-1)}
+      >
+        Return
+      </Button>
     </VStack>
-  )
+  );
 }
 
-export default GraphPage
+export default GraphPage;
